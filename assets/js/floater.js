5fc669d104d0972de0b129e0f47c1b1199893056
minus5saihada <minus5saihada@gmail.com>
2022-04-01 03:04:25 +0900

    SP時にfloater.jsが機能しないよう修正

--- assets/js/floater.js
+++ assets/js/floater.js
@@ -1,11 +1,13 @@
 $(function() {
   $(window).scroll(function() {
     let offset = $(this).scrollTop() / -5;
-    // $('.top-parallax__img').css('transform', 'translateY(' + offset + 'px)' );
-    $('.js-floater').eq(0).css('background-position', '0px ' + offset + 'px');
-    $('.js-floater').eq(1).css('background-position', '0px ' + offset * 0.85 + 'px');
-    $('.js-floater').eq(2).css('background-position', '0px ' + offset * 0.7 + 'px');
-    $('.js-floater').eq(3).css('background-position', '0px ' + offset * 0.55 + 'px');
-    $('.js-floater').eq(4).css('background-position', '0px ' + offset * 0.4 + 'px');
+    if ($(window).width() >= 768) {
+      // $('.top-parallax__img').css('transform', 'translateY(' + offset + 'px)' );
+      $('.js-floater').eq(0).css('background-position', '0px ' + offset + 'px');
+      $('.js-floater').eq(1).css('background-position', '0px ' + offset * 0.85 + 'px');
+      $('.js-floater').eq(2).css('background-position', '0px ' + offset * 0.7 + 'px');
+      $('.js-floater').eq(3).css('background-position', '0px ' + offset * 0.55 + 'px');
+      $('.js-floater').eq(4).css('background-position', '0px ' + offset * 0.4 + 'px');
+    }
   });
 });
