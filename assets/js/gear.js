c3f87fef281e61d7b9cc3758900cd3fdcdcbb4cb
minus5saihada <minus5saihada@gmail.com>
2022-04-02 01:57:46 +0900

    ページを最後までスクロールした際の歯車の動きを追加

--- assets/js/gear.js
+++ assets/js/gear.js
@@ -7,5 +7,33 @@ $(function() {
     } else {
       $('.gear__img').removeClass('gear__img--scroll');
     }
+    // 最下部アニメーション用
+    let scrollTop = $(this).scrollTop(); // スクロール量
+    let wholeheight = Math.max(document.body.clientHeight, document.body.scrollHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight); // ページ全体の高さ
+    let windowHeight = window.innerHeight; // 表示領域の高さ
+    let bottomPoint = wholeheight - windowHeight; // 最下部判定用
+    let firstGear = $('.gear__img--first').height(); // アニメーション歯車の高さ
+    let firstAnimationOffset = wholeheight - firstGear; // 最下層についた際に付与する高さ
+    let tenthGear = $('.gear__img--tenth').height(); // アニメーション歯車の高さ
+    let tenthAnimationOffset = wholeheight - tenthGear; // 最下層についた際に付与する高さ
+    if(scrollTop == bottomPoint) {
+      document.documentElement.style.setProperty('--first-animation-offset', firstAnimationOffset);
+      document.documentElement.style.setProperty('--tenth-animation-offset', tenthAnimationOffset);
+      $('.gear__img--first').addClass('gear__img--first-to-bottom');
+      setTimeout(() => {
+        $('.gear__img--first').addClass("gear__img--rotate");
+      }, 1000);
+      $('.gear__img--tenth').addClass('gear__img--tenth-to-bottom');
+      setTimeout(() => {
+        $('.gear__img--first').addClass("gear__img--gold");
+        $('.gear__img--tenth').addClass("gear__img--gold");
+      }, 3200);
+    } else {
+      $('.gear__img--first').removeClass('gear__img--first-to-bottom');
+      $('.gear__img--first').removeClass("gear__img--rotate");
+      $('.gear__img--tenth').removeClass('gear__img--tenth-to-bottom');
+      $('.gear__img--first').removeClass("gear__img--gold");
+      $('.gear__img--tenth').removeClass("gear__img--gold");
+    }
   });
 });
