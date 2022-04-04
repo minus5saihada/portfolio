25c3ba0c899138d1bb5edbb86037f063f6437d4c
minus5saihada <minus5saihada@gmail.com>
2022-04-05 00:05:14 +0900

    プログレスバー：アニメーション追加

---
+++ assets/js/animation.js
@@ -0,0 +1,72 @@
+$(function() {
+  $('.js-html-pug').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--html-pug');
+    }
+  });
+  $('.js-css-sass-bem').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--css-sass-bem');
+    }
+  });
+  $('.js-javascript-jquery-vue-react').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--javascript-jquery-vue-react');
+    }
+  });
+  $('.js-xd-ai-ps-figma').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--xd-ai-ps-figma');
+    }
+  });
+  $('.js-php-laravel').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--php-laravel');
+    }
+  });
+  $('.js-java-spring').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--java-spring');
+    }
+  });
+  $('.js-shellscript').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--shellscript');
+    }
+  });
+  $('.js-sql').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--sql');
+    }
+  });
+  $('.js-docker').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--docker');
+    }
+  });
+  $('.js-wordpress').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--wordpress');
+    }
+  });
+  $('.js-git-github-svn').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--git-github-svn');
+    }
+  });
+  $('.js-gulp-npm-webpack').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--gulp-npm-webpack');
+    }
+  });
+  $('.js-linux').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--linux');
+    }
+  });
+  $('.js-apache-nginx').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('skills__progress-bar--apache-nginx');
+    }
+  });
+});
