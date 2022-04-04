388ae9ac690d487df449927f601a7e103b00160e
minus5saihada <minus5saihada@gmail.com>
2022-04-05 00:40:58 +0900

    リンク押下時ハンバーガーメニュークローズ

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -5,4 +5,10 @@ $(function() {
     $('.header__link').toggleClass('header__link--open');
     $('.header__img').toggleClass('header__img--open');
   });
+  $('.header__link').on('click', function () {
+    $('.header__line').toggleClass('header__line--open');
+    $('.header__menu').toggleClass('header__menu--open');
+    $('.header__link').toggleClass('header__link--open');
+    $('.header__img').toggleClass('header__img--open');
+  });
 });
